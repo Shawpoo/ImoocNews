30ef67179106b34132238f79d8b2a37a99b735fe
wuxiaopeng <wuxiaopeng@comisys.net>
2021-05-16 23:29:19 +0800

    [feature] 增加首页搜索框

---
+++ components/titlebar/titlebar.vue
@@ -0,0 +1,46 @@
+<template>
+	<view class="search-box">
+		<view class="search-content">
+			<uni-icons class="search-box-icon" type="search"></uni-icons>
+			<text class="search-box-text">html vue</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "titlebar",
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.search-box {
+		height: 96rpx;
+		box-sizing: border-box;
+		width: 100%;
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		padding: 19rpx 20rpx;
+		background-color: $theme-color;
+
+		.search-content {
+			background-color: #fff;
+			border-radius: 30px;
+			padding: 10rpx 20rpx;
+
+			.search-box-icon {
+				padding: 5rpx 5rpx;
+				margin-right: 16rpx;
+			}
+
+			.search-box-text {}
+		}
+	}
+</style>
---
+++ pages/tabbar/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<view class="page-index">
+		<titlebar></titlebar>
+		<view>
+			<view v-for="(item, index) in 100" :key="index">{{index}} xxxxx</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	// easyCom
+	// import titlebar from "@/components/titlebar/titlebar.vue";
+	export default {
+		// components: {
+		// 	titlebar
+		// },
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.page-index {
+		padding-top: 96rpx;
+	}
+</style>

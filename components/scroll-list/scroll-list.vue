00966234475d8f702d9181b46692cd0f88d978d2
wuxiaopeng <994227083@qq.com>
2021-05-18 21:43:00 +0800

    [feature] 封装滑动的list组件

---
+++ components/scroll-list/scroll-list.vue
@@ -0,0 +1,34 @@
+<template>
+	<view class="scroll">
+		<scroll-view class="scroll-list" scroll-y="true">
+			<view>
+				<slot></slot>
+			</view>
+		</scroll-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "scroll-list",
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.scroll {
+		flex: 1;
+		overflow: hidden;
+		box-sizing: border-box;
+
+		.scroll-list {
+			height: 100%;
+			display: flex;
+			flex-direction: column;
+		}
+	}
+</style>
--- pages/tabbar/index/index.vue
+++ pages/tabbar/index/index.vue
@@ -2,15 +2,11 @@
 	<view class="page-index">
 		<titlebar></titlebar>
 		<tab :list="tabList" @clickTab="clickTab"></tab>
-		<view class="scroll">
-			<scroll-view class="scroll-list" scroll-y="true">
-				<view>
-					<view>
-						<view v-for="(item, index) in 100" :key="index">{{index}} xxxxx</view>
-					</view>
-				</view>
-			</scroll-view>
-		</view>
+		<scroll-list>
+			<view>
+				<view v-for="(item, index) in 100" :key="index">{{index}} xxxxx</view>
+			</view>
+		</scroll-list>
 	</view>
 </template>
 
@@ -62,17 +58,5 @@
 		padding-top: 96rpx;
 		flex-direction: column;
 		flex: 1;
-
-		.scroll {
-			flex: 1;
-			overflow: hidden;
-			box-sizing: border-box;
-			
-			.scroll-list {
-				height: 100%;
-				display: flex;
-				flex-direction: column;
-			}
-		}
 	}
 </style>
